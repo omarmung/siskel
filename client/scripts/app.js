6a8bff4b966590ed396cc3a3dc9f416bce29a458
omarmung <dkimmel@gmail.com>
2016-08-10 17:40:45 -0700

    completed all tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,11 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    if (this.get('like') === true) {
+      this.set('like', false);
+    } else {
+      this.set('like', true);  
+    }
   }
 
 });
@@ -16,13 +20,16 @@ var Movies = Backbone.Collection.extend({
 
   initialize: function() {
     // your code here
+    this.on('change', this.sort, this);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
     // your code here
-  }
+    this.comparator = field;
+    this.sort();
+  },
 
 });
 
@@ -59,6 +66,7 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.model.on('change', this.render, this);
   },
 
   events: {
@@ -67,6 +75,7 @@ var MovieView = Backbone.View.extend({
 
   handleClick: function() {
     // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +88,9 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    // your code here CHECK THIS
+    this.render();
+    this.collection.on('change', this.render, this);
   },
 
   render: function() {
